e1176bf373d3c6f87dd724a357643fd0944367c5
mnajborowski <michal.najborowski@gmail.com>
2022-08-24 00:38:54 +0200

    add custom form via Kotlin HTML DSL

--- src/main/kotlin/com/example/common/infrastructure/security/authorization/AuthorizationRouting.kt
+++ src/main/kotlin/com/example/common/infrastructure/security/authorization/AuthorizationRouting.kt
@@ -5,12 +5,39 @@ import com.example.common.infrastructure.security.principal.Role.WRITE
 import com.example.common.infrastructure.security.principal.UserSession
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.html.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
+import kotlinx.html.body
+import kotlinx.html.br
+import kotlinx.html.button
+import kotlinx.html.getForm
+import kotlinx.html.label
+import kotlinx.html.passwordInput
+import kotlinx.html.textInput
 
 fun Application.configureAuthorizationRouting() {
     routing {
+        get("/login-form") {
+            call.respondHtml {
+                body {
+                    getForm("/login") {
+                        label { text("Username") }
+                        br
+                        textInput(name = "username")
+                        br
+                        br
+                        label { text("Password") }
+                        br
+                        passwordInput(name = "password")
+                        br
+                        button { text("Login") }
+                    }
+                }
+            }
+        }
+
         authenticate("auth-form") {
             get("/login") {
                 val userName = call.principal<UserIdPrincipal>()?.name.toString()
--- src/main/kotlin/com/example/common/infrastructure/security/authorization/DigestConfiguration.kt
+++ src/main/kotlin/com/example/common/infrastructure/security/authorization/DigestConfiguration.kt
@@ -2,21 +2,15 @@ package com.example.common.infrastructure.security.authorization
 
 import io.ktor.util.*
 import java.security.MessageDigest
-import kotlin.random.Random
 
 object DigestConfiguration {
     val digestFunctionStatic =
         getDigestFunction("SHA-256") { "ktor${it.length}" }
-    val digestFunctionRandom =
-        getDigestFunction("SHA-256") { Random.nextBytes(32).toString() }
-    fun digestFunctionCustom(salt: ByteArray) =
-        getDigestFunction("SHA-256") { salt.toString() }
-
-
     fun sha256(text: String, salt: ByteArray): ByteArray =
         with(MessageDigest.getInstance("SHA-256")) {
             update(salt)
             digest(text.toByteArray())
         }
-    fun md5(text: String): ByteArray = MessageDigest.getInstance("MD5").digest(text.toByteArray())
+    fun md5(text: String): ByteArray =
+        MessageDigest.getInstance("MD5").digest(text.toByteArray())
 }
