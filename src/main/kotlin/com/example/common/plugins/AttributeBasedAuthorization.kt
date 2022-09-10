2db39f6f727745f6562328eb6156be4784c714d1
mnajborowski <michal.najborowski@gmail.com>
2022-09-10 23:55:47 +0200

    create AttributeBasedAuthorization

---
+++ src/main/kotlin/com/example/common/plugins/AttributeBasedAuthorization.kt
@@ -0,0 +1,24 @@
+package com.example.common.plugins
+
+import com.example.common.CityId
+import com.example.common.infrastructure.security.principal.UserSession
+import com.example.user.domain.service.UserService
+import io.ktor.application.*
+import io.ktor.routing.*
+import io.ktor.sessions.*
+import org.koin.ktor.ext.inject
+import javax.naming.AuthenticationException
+
+fun Route.livesInCity(
+    cityId: CityId,
+    build: Route.() -> Unit
+): Route {
+    val service: UserService by inject()
+    return authorize(listOf("auth-lives-in-city"), build) {
+        val session = call.sessions.get<UserSession>()
+            ?: throw AuthenticationException("Session not found")
+        val user = service.getUser(session.name)
+        if (user.city.id != cityId)
+            throw SecurityException("User ${user.username} doesn't live in ${user.city.name}.")
+    }
+}
